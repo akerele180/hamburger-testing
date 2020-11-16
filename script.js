847566f0e18ae7fd69520e7c3bc691f0c9fae093
Akerele, Tosin James <akerele.james1699@gmail.com>
2020-11-16 16:39:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const hamburgerCancel = document.getElementById('cancel');
+const hamburger = document.getElementById('hamburger');
+const dropDown = document.querySelector('.lists');
+
+
+
+hamburger.addEventListener('click', ()=>{
+    hamburger.style.display = 'none';
+    hamburgerCancel.style.display = 'block';
+    dropDown.style.display = 'block';
+    // dropDown.style = 'top 2s ease';
+})
+
+hamburgerCancel.addEventListener('click', ()=>{
+    hamburger.style.display = 'block';
+    hamburgerCancel.style.display = 'none';
+    dropDown.style.display='none';
+    dropDown.style.transition = 'top 1s ease';
+})
